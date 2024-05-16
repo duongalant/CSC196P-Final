4086d093af70854d7e231f7836262d831e7fc6e2
Alan Duong <alantduong@csus.edu>
2024-05-16 01:49:35 -0700

    Basic reflex game

--- app/src/main/java/alantduong/com/csc196p_final/MainActivity.kt
+++ app/src/main/java/alantduong/com/csc196p_final/MainActivity.kt
@@ -1,20 +1,60 @@
 package alantduong.com.csc196p_final
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var relativeLayout: RelativeLayout
+
+    // Runnable function
+    private val runnable = Runnable {
+        // Set the background on the screen
+        relativeLayout.setBackgroundResource(R.color.green)
+
+        // Get the system time in millisecond when the screen background is set
+        val time = System.currentTimeMillis()
+
+        // Function when stop button is clicked
+        stopButton.setOnClickListener {
+            // get the system time in millisecond when the stop button is clicked
+            val time1 = System.currentTimeMillis()
+
+            // Display reflex time in toast message
+            Toast.makeText(
+                applicationContext,
+                "Your reflex time is: ${time1 - time}ms",
+                Toast.LENGTH_LONG
+            ).show()
+
+            // Remove the background again
+            relativeLayout.setBackgroundResource(0)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        relativeLayout = findViewById(R.id.relativeLayout)
+        startButton = findViewById(R.id.startButton)
+        stopButton = findViewById(R.id.stopButton)
+
+        // Function when the start button is clicked
+        startButton.setOnClickListener {
+            // Generate a random number from 1-10
+            val num = Random.nextInt(1, 5)
+
+            // Call the runnable function after a post delay of num seconds
+            val handler = Handler()
+            handler.postDelayed(runnable, num * 1000L)
         }
     }
 }
